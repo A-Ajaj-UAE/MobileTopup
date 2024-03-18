780bca228d323457021b084fc968290998a1a823
Ahmad Ajaj <ahmad@bamboo-card.com>
2024-03-19 01:49:52 +0400

    update unit test.

--- MobileTopup.UnitTests/Services/UserServiceTests.cs
+++ MobileTopup.UnitTests/Services/UserServiceTests.cs
@@ -13,7 +13,7 @@ namespace MobileTopup.UnitTests.Services
     public class UserServiceTest
     {
         private readonly UserService _userService;
-        private readonly Mock<UserRepository> _userRepository;
+        private readonly Mock<IUserRepository> _userRepository;
         private readonly Mock<HttpClient> httpClient;   
         private readonly Mock<ILogger<UserService>> logger;   
         private readonly Mock<IMapper> mapper;   
@@ -22,7 +22,7 @@ namespace MobileTopup.UnitTests.Services
         {
             var userValidator = new UserValidator();
             var BenficiaryValidator = new BeneficiaryValidator();
-            _userRepository = new Mock<UserRepository>();
+            _userRepository = new Mock<IUserRepository>();
             httpClient = new Mock<HttpClient>();
             logger = new Mock<ILogger<UserService>>();
             mapper = new Mock<IMapper>();
@@ -37,20 +37,19 @@ namespace MobileTopup.UnitTests.Services
             var user = new User { IsVerified = true };
             user.Beneficiaries = new List<Beneficiary>
             {
-                new Beneficiary("Beneficiary 1",true),
-                new Beneficiary("Beneficiary 2",true),
-                new Beneficiary("Beneficiary 3",true),
-                new Beneficiary("Beneficiary 4",true),
+                new Beneficiary("Beneficiary 1", true),
+                new Beneficiary("Beneficiary 2", true),
+                new Beneficiary("Beneficiary 3", true),
+                new Beneficiary("Beneficiary 4", true),
             };
 
             var beneficiaryRequst = new AddBeneficiaryRequest { NickName = "Beneficiary 5", IsActive = true };
-            var beneficiary = new Beneficiary { NickName = "Beneficiary 5", IsActive = true };
 
             // Act
             _userService.AddBeneficiary(user, beneficiaryRequst);
-       
+
             // Assert
-            Assert.Contains(beneficiary, user.Beneficiaries);
+            Assert.Contains(beneficiaryRequst.NickName, user.Beneficiaries.Select(b => b.NickName));
         }
 
         [Fact]
@@ -68,11 +67,13 @@ namespace MobileTopup.UnitTests.Services
             };
 
             var beneficiaryRequst = new AddBeneficiaryRequest { NickName = "Beneficiary 6", IsActive = true };
-            var beneficiary = new Beneficiary { NickName = "Beneficiary 6", IsActive = true };
 
             // Act & Assert
             var exception = Assert.Throws<ValidationException>(() => _userService.AddBeneficiary(user, beneficiaryRequst));
-            Assert.IsType(typeof(ValidationException), exception);
+            
+            Assert.IsType<ValidationException>(exception);
+
+            //validate error is BeneficiaryExceptions.MaxActiveBenfenciryExceeded
             Assert.Contains(BeneficiaryExceptions.MaxActiveBenfenciryExceeded, exception.Errors.Select(e=> e.ErrorMessage));
         }
 
@@ -87,7 +88,9 @@ namespace MobileTopup.UnitTests.Services
 
             // Act & Assert
             var exception = Assert.Throws<ValidationException>(() => _userService.AddBeneficiary(user, beneficiaryRequst));
-            Assert.IsType(typeof(ValidationException), exception);
+            
+            Assert.IsType<ValidationException>(exception);
+
             Assert.Contains(BeneficiaryExceptions.BenefenciryNickNameLenghtExceeded, exception.Errors.Select(e=> e.ErrorMessage));
         }
 
@@ -104,7 +107,8 @@ namespace MobileTopup.UnitTests.Services
             _userService.AddBeneficiary(user, beneficiary);
 
             // Assert
-            Assert.Single(user.Beneficiaries, beneficiary);
+            Assert.Contains(beneficiary.NickName, (user.Beneficiaries.Select(b => b.NickName)));
+            
         }
 
         
